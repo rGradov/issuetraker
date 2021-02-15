a9cc6387079006246441707347e9c3a87ce7046e
rGradov <VladRudenualu@yandex.by>
2021-02-15 13:00:40 +0300

    initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DataService} from './data/data.service';
+import {MainViewModule} from './mainview/mainview.module';
+import {AuthInterceptor} from './auth.interceptor';
+import {AuthModule} from './auth/auth.module';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MainViewModule,
+    AuthModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(DataService,{delay:500}),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
